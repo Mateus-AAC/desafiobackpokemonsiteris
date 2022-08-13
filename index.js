3bea7cc57f1d9da014132977343dbc5348e6ba1c
mateus augusto <vhsdoidao@gmail.com>
2022-08-13 17:57:35 -0300

    melhorando algumas coisas

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ const app = require('./src/main');
 const port = normalizaPort(process.env.PORT || 3000);
 
 function normalizaPort(val) {
+
     const port = parseInt(val, 10);
+
     if (isNaN(port)) {
+
         return val;
+
     }
     if (port >= 0) {
+
         return port;
+
     }
+
     return false;
+
 }
 
 app.listen(port, function () {
-    console.log(`rodando na porta: ${port}`)
+
+    console.log(`rodando na porta: ${port}`);
+    
 })
